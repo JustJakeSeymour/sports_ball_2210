f601099305ad5b5e8342344592372369cf6be939
J Seymour <JustJakeSeymour@gmail.com>
2022-10-22 11:07:05 -0600

    write iteration 3 tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -45,4 +45,72 @@ RSpec.describe Team do
     expect(team.roster).to eq [player_1, player_2]
   end
 
+  it 'has a different player count when players added' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous", 1000000, 36)
+    player_2 = Player.new("Kenny Denunez", 500000, 24)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+
+    expect(team.player_count).to eq 2
+  end
+
+  it 'knows long term player contracts' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous", 1000000, 36)
+    player_2 = Player.new("Kenny Denunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.long_term_players).to eq [player_1, player_3]
+  end
+
+  it 'knows short term player contracts' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous", 1000000, 36)
+    player_2 = Player.new("Kenny Denunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.short_term_players).to eq [player_2, player_4]
+  end
+  it 'can calculate total value' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous", 1000000, 36)
+    player_2 = Player.new("Kenny Denunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+
+    expect(team.total_value).to eq 85200000
+  end
+
+  it 'gives team details' do
+    team = Team.new("Dodgers", "Los Angeles")
+    player_1 = Player.new("Michael Palledorous", 1000000, 36)
+    player_2 = Player.new("Kenny Denunez", 500000, 24)
+    player_3 = Player.new("Alan McClennan", 750000, 48)
+    player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    team.add_player(player_1)
+    team.add_player(player_2)
+    team.add_player(player_3)
+    team.add_player(player_4)
+    
+  end
 end
